9d90c003e8c77ef037a478a32071cb9e4e8cbf94
rafapereirabr <rafa.pereira.br@gmail.com>
2020-08-10 17:22:56 -0300

    1st draft download_r5.R

--- r-package/R/download_r5.R
+++ r-package/R/download_r5.R
@@ -1,11 +1,11 @@
-#' Download R5 .jar file
-#'
-#' The function downloads a compiled version of R5 (a j.ar file) and
+#' Download R5 Jar file
+#' 
+#' @description Download a compiled version of R5 (a jar file) and
 #' saves it locally. This is a compilation of R5 tailored for the
 #' purposes of the package r5r but that keeps R5's essential features.
 #'
 #' @export
-#' @family general support functions
+#' @family setup
 #' @examples \donttest{
 #'
 #' library(r5r)
@@ -14,22 +14,59 @@
 #'
 #' }
 #'
-download_r5 <- function(version){
+download_r5 <- function(path, version){
 
   message(paste0('Downloading R5 version', version))
 
-  # Get metadata with data addresses
-  tempf <- file.path(tempdir(), "metadata.csv")
+  # # Get metadata with data addresses
+  # tempf <- file.path(tempdir(), "metadata.csv")
+  # 
+  # # check if metadata has already been downloaded
+  # if (file.exists(tempf)) {
+  #   metadata <- utils::read.csv(tempf, stringsAsFactors=F)
+  # 
+  # } else {
+  #   # download it and save to metadata
+  #   httr::GET(url="http://www.ipea.gov.br/geobr/metadata/metadata_gpkg.csv", httr::write_disk(tempf, overwrite = T))
+  #   metadata <- utils::read.csv(tempf, stringsAsFactors=F)
+  # }
+  # 
+  # return(metadata)
+  }
 
-  # check if metadata has already been downloaded
-  if (file.exists(tempf)) {
-    metadata <- utils::read.csv(tempf, stringsAsFactors=F)
 
+
+download_r5 <- function(path = NULL,
+                       version = "1.4.0",
+                       file_name = paste0("r5-", version, "-shaded.jar"),
+                       url = "http://www.ipea.gov.br/geobr/metadata/metadata_gpkg.csv",
+                       quiet = FALSE,
+                       cache = TRUE) {
+  if (cache) {
+    # Check we can find the package
+    libs <- .libPaths()[1]
+    if (!checkmate::test_directory_exists(file.path(libs, "r5r"))) {
+      cache <- FALSE
+    }
+  }
+  
+  if (cache) {
+    # Check for JAR folder can find the package
+    if (!checkmate::test_directory_exists(file.path(libs, "r5r", "jar"))) {
+      dir.create(file.path(libs, "r5r", "jar"))
+    }
+    destfile <- file.path(libs, "r5r", "jar", file_name)
+    if (checkmate::test_file_exists(destfile)) {
+      message("Using cached version from ", destfile)
+      return(destfile)
+    }
   } else {
-    # download it and save to metadata
-    httr::GET(url="http://www.ipea.gov.br/geobr/metadata/metadata_gpkg.csv", httr::write_disk(tempf, overwrite = T))
-    metadata <- utils::read.csv(tempf, stringsAsFactors=F)
+    checkmate::assert_directory_exists(path)
+    destfile <- file.path(path, file_name)
   }
-
-  return(metadata)
-  }
+  
+ # UNCOMMENT url <- paste0(url, "/", version, "/otp-", version, "-shaded.jar")
+  message("The OTP will be saved to ", destfile)
+  utils::download.file(url = url, destfile = destfile, mode = "wb", quiet = quiet)
+  return(destfile)
+}
