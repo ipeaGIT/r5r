d8295c864e2e1f5642386429df1a9f1b5a0f2e83
Marcus Saraiva <marcus.saraiva@gmail.com>
2020-08-11 18:51:55 -0300

    Added loadDestinationPointsFromCsv method to r5r api, and succesfully called it from R

--- java-api/src/R5RCore.java
+++ java-api/src/R5RCore.java
@@ -1,12 +1,15 @@
+import com.conveyal.r5.analyst.FreeFormPointSet;
 import com.conveyal.r5.kryo.KryoNetworkSerializer;
 import com.conveyal.r5.transit.TransportNetwork;
+import com.conveyal.r5.util.CSVInputStreamProvider;
 
 import java.io.File;
 import java.io.IOException;
 
 public class R5RCore {
 
-    TransportNetwork transportNetwork;
+    private TransportNetwork transportNetwork;
+    private FreeFormPointSet destinationPointSet;
 
     public R5RCore(String dataFolder) {
         File file = new File(dataFolder + "network.dat");
@@ -25,6 +28,16 @@ public class R5RCore {
 //            transportNetwork.readOSM(new File(dir, "osm.mapdb"));
             transportNetwork.transitLayer.buildDistanceTables(null);
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadDestinationPointsFromCsv(String csvFile) {
+        CSVInputStreamProvider csvInputStream;
+        try {
+            csvInputStream = new CSVInputStreamProvider(csvFile);
+            destinationPointSet = FreeFormPointSet.fromCsv(csvInputStream, "lat", "lon", "id", "count");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
