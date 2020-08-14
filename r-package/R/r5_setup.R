2b10b9b5c33643b0077aef86fa11b536cb61bd28
rafapereirabr <rafa.pereira.br@gmail.com>
2020-08-14 17:12:41 -0300

    fix setup to read r5 from package diretory

--- r-package/R/r5_setup.R
+++ r-package/R/r5_setup.R
@@ -4,7 +4,6 @@
 #'
 #'
 #'
-#' @param r5_path
 #' @param data_path
 #'
 #' @return
@@ -12,9 +11,19 @@
 #'
 #' @examples
 #'
-r5_setup <- function(r5_path, data_path) {
+r5_setup <- function(data_path, version='4.9.0') {
+
+
+  # jar file
+  jar_file <- file.path(.libPaths()[1], "r5r", "jar", paste0("r5r_v", version, ".jar"))
+
+  if (checkmate::test_file_exists(jar_file)) {
+    message("Using cached version from ", jar_file)
+  } else { download_r5(version='4.9.0') }
+
+
   rJava::.jinit()
-  rJava::.jaddClassPath(path = paste0(r5_path, "R5.jar"))
+  rJava::.jaddClassPath(path = jar_file)
   # rJava::.jaddClassPath(path = paste0(r5_path, "r5r_core.jar"))
 
   r5r_core <- rJava::.jnew("com.conveyal.r5.R5RCore", data_path)
