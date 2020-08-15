24b713b30e3ddb1a3dadcb2500818a18e88633ac
rafapereirabr <rafa.pereira.br@gmail.com>
2020-08-14 21:06:14 -0300

    closes #7

--- r-package/R/setup_r5.R
+++ r-package/R/setup_r5.R
@@ -26,24 +26,20 @@
 
 setup_r5 <- function(data_path, version='4.9.0') {
 
-  # # check directory input
-  # is.null(data_path){ stop(paste0("Please provide data_path")) }
+  # check directory input
+  if(is.null(data_path)){ stop(paste0("Please provide data_path")) }
 
   # check if data_path has osm.pbf and gtfs data
   any_pbf <- sum(list.files(data_path) %like% '.pbf') > 0
   any_gtfs <- sum(list.files(data_path) %like% '.zip') > 0
 
+  # stop if there is no input data
   if ( any_pbf==FALSE & any_gtfs==FALSE){
     stop(paste0("No street network data (.pbf) and no public transport data
                 (gtfs) provided"))}
 
-  if ( any_pbf==TRUE & any_gtfs==FALSE){
-    stop(paste0("No public transport data (gtfs) provided. Graph will be
-                built with the street network only."))}
-
   if ( any_pbf==FALSE & any_gtfs==TRUE){
-    stop(paste0("No street network data (.pbf) provided. Graph will be
-                built with public transport data (gtfs) only."))}
+    stop(paste0("\nAn OSM PBF file is required to build a network."))}
 
   # path to jar file
   jar_file <- file.path(.libPaths()[1], "r5r", "jar", paste0("r5r_v", version, ".jar"))
@@ -57,6 +53,11 @@ setup_r5 <- function(data_path, version='4.9.0') {
   rJava::.jinit()
   rJava::.jaddClassPath(path = jar_file)
   r5r_core <- rJava::.jnew("com.conveyal.r5.R5RCore", data_path)
+
+  # Warning message if there is only one of either OSM or GTFS data set
+  if ( any_pbf==TRUE & any_gtfs==FALSE){
+    message(paste0("\nNo public transport data (gtfs) provided. Graph will be
+                built with the street network only."))}
 
   return(r5r_core)
 }
--- r-package/tests/testthat/test-setup_r5.R
+++ r-package/tests/testthat/test-setup_r5.R
@@ -5,19 +5,17 @@ path <- system.file("extdata", package = "r5r")
 # expected behavior
 test_that("download_r5- expected behavior", {
 
-  testthat::expect_message( r5_setup(data_path = path) )
-  testthat::expect_message(r5_setup(data_path = path, version='4.9.0'))
+  testthat::expect_message( setup_r5(data_path = path) )
+  testthat::expect_message(setup_r5(data_path = path, version='4.9.0'))
 
 })
 
 
-# # Expected errors
-# test_that("download_r5 - expected errors", {
-#
-#   testthat::expect_error( r5_setup(data_path = NULL) )
-#
-#
-#   testthat::expect_error( r5_setup(data_path = path, version = "0") )
-#   testthat::expect_error( download_r5(version = NULL ) )
-#
-#   })
+# Expected errors
+test_that("download_r5 - expected errors", {
+
+  testthat::expect_error( setup_r5(data_path = NULL) )
+  testthat::expect_error( setup_r5(data_path = path, version = "0") )
+  testthat::expect_error( download_r5(version = NULL ) )
+
+  })
