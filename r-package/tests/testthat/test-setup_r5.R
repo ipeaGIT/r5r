b13248a9b743b2809c5b6d40789b4df25668979f
rafapereirabr <rafa.pereira.br@gmail.com>
2020-08-16 14:33:58 -0300

    improve test-setup_r5.R

--- r-package/tests/testthat/test-setup_r5.R
+++ r-package/tests/testthat/test-setup_r5.R
@@ -8,14 +8,28 @@ test_that("download_r5- expected behavior", {
   testthat::expect_message( setup_r5(data_path = path) )
   testthat::expect_message(setup_r5(data_path = path, version='4.9.0'))
 
+  # remove files GTFS
+  file.remove( file.path(path, "poa.zip") )
+  testthat::expect_message( setup_r5(data_path = path) )
+
 })
+
+
+
 
 
 # Expected errors
 test_that("download_r5 - expected errors", {
 
   testthat::expect_error( setup_r5(data_path = NULL) )
-  testthat::expect_error( setup_r5(data_path = path, version = "0") )
   testthat::expect_error( download_r5(version = NULL ) )
+
+  # remove existing network.dat
+  file.remove( file.path(path, "network.dat") )
+  testthat::expect_error( setup_r5(data_path = path, version = "0") )
+
+  # remove files GTFS
+  file.remove( file.path(path, c("poa_osm.pbf" , "poa_osm.pbf.mapdb", "poa_osm.pbf.mapdb.p" )) )
+  testthat::expect_error( setup_r5(data_path = path) )
 
   })
--- r-package/tests_rafa/test_rafa.R
+++ r-package/tests_rafa/test_rafa.R
@@ -22,15 +22,32 @@ devtools::document(pkg = ".")
 
 # build transport network
 path <- system.file("extdata", package = "r5r")
+list.files(path)
+list.files(file.path(.libPaths()[1], "r5r", "jar"))
+
+# remove files
+# file.remove( file.path(path, "network.dat") )
+# file.remove( file.path(.libPaths()[1], "r5r", "jar", "r5r_v4.9.0.jar") )
+
 r5_core <- setup_r5(data_path = path)
+
+create core
+
 
 # load origin/destination points
 points <- read.csv(system.file("extdata/poa_hexgrid.csv", package = "r5r"))[1:5,]
 points_sf <- sfheaders::sf_multipoint(points, x='lon', y='lat', multipoint_id = 'id')
 
+jdx::convertToR(travel_times)
+a <- read.table("C:/Users/user/Documents/R/win-library/4.0/r5r/extdata/network.dat")
+
+a <- jdx::convertToR(a$V1)
+
+
+
+
 
 ##### TESTS detailed_itineraries ------------------------
-
 
 # input
 fromLat <- points[1,]$lat
@@ -44,7 +61,6 @@ direct_modes <- c("WALK", "BICYCLE", "CAR")
 transit_modes <-"BUS"
 max_street_time = 30L
 
-
 system.time(
 trip <- detailed_itineraries( fromLat = fromLat,
                               fromLon = fromLon,
@@ -57,6 +73,12 @@ trip <- detailed_itineraries( fromLat = fromLat,
                               transit_modes = transit_modes,
                               max_street_time = max_street_time,
                               shortest_path = F) )
+
+
+
+
+
+
 
 
 ##### TESTS multiple detailed_itineraries ------------------------
@@ -85,31 +107,35 @@ trips <- multiple_detailed_itineraries( r5_core,
 ))
 
 
+
+
+
+
 ##### TESTS travel_time_matrix ------------------------
+options(java.parameters = "-Xmx16G")
+
+# input
+origins <- destinations <-  read.csv(system.file("extdata/poa_hexgrid.csv", package = "r5r"))[1:5,]
 
  # input
-origins <- destinations <- points[1:500,]
-
- trip_date <- "2019-05-20"
- departure_time <- "14:00:00"
- street_time = 15L
  direct_modes <- c("WALK", "BICYCLE", "CAR")
  transit_modes <-"BUS"
+ departure_time <- "14:00:00"
+ trip_date <- "2019-05-20"
+ street_time = 15L
  max_street_time = 30L
  max_trip_duration = 300L
 
-
- system.time(
- tt <- travel_time_matrix( r5_core,
-                     origins,
-                     destinations,
-                     direct_modes,
-                     transit_modes,
-                     trip_date,
-                     departure_time,
-                     max_street_time,
-                     max_trip_duration)
- )
+ df <- travel_time_matrix( r5_core = r5_core,
+                           origins = points,
+                           destinations = points,
+                           trip_date = trip_date,
+                           departure_time = departure_time,
+                           direct_modes = direct_modes,
+                           transit_modes = transit_modes,
+                           max_street_time = max_street_time,
+                           max_trip_duration = max_trip_duration
+                           )
 
 
  head(tt)
@@ -117,12 +143,13 @@ origins <- destinations <- points[1:500,]
 
  1474469/ 143.64
  245480 /32.74
+ 523074 / 46.96
 
 ##### Coverage ------------------------
 
 # each function separately
-function_coverage(fun=r5r::download_r5, test_file("tests/testthat/test-download_r5.R"))
-function_coverage(fun=r5r::setup_r5, test_file("tests/testthat/test-setup_r5.R"))
+covr::function_coverage(fun=r5r::download_r5, test_file("tests/testthat/test-download_r5.R"))
+covr::function_coverage(fun=r5r::setup_r5, test_file("tests/testthat/test-setup_r5.R"))
 
 # the whole package
 covr::package_coverage(path = ".", type = "tests")
