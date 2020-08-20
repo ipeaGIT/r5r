6e023fa8eb5843129bea1e2bc3978d0f2231c912
rafapereirabr <rafa.pereira.br@gmail.com>
2020-08-20 09:33:46 -0300

    cleaning test-travel_time_matrix.R

--- r-package/tests/testthat/test-travel_time_matrix.R
+++ r-package/tests/testthat/test-travel_time_matrix.R
@@ -1,12 +1,12 @@
 context("travel_time_matrix")
 
-# setup_r5
-path <- system.file("extdata", package = "r5r")
-r5r_core <- setup_r5(data_path = path)
-
 # load origin/destination points
 points <- read.csv(system.file("extdata/poa_hexgrid.csv", package = "r5r"))[1:5,]
 points_sf <- sfheaders::sf_multipoint(points, x='lon', y='lat', multipoint_id = 'id')
+
+# setup_r5
+path <- system.file("extdata", package = "r5r")
+r5r_core <- setup_r5(data_path = path)
 
 # input
 mode <- "WALK"
@@ -14,21 +14,23 @@ departure_time <- "14:00:00"
 trip_date <- "2019-05-20"
 max_trip_duration = 300L
 
-df <- travel_time_matrix( r5r_core = r5r_core,
+df <- travel_time_matrix( r5r_core,
                           origins = points,
                           destinations = points,
-                          trip_date = trip_date,
-                          departure_time = departure_time,
+                          trip_date,
+                          departure_time,
                           mode = mode,
-                          max_trip_duration = max_trip_duration)
+                          max_trip_duration,
+                          verbose = FALSE)
 
-df_sf <- travel_time_matrix( r5r_core = r5r_core,
+df_sf <- travel_time_matrix( r5r_core,
                           origins = points_sf,
                           destinations = points_sf,
-                          trip_date = trip_date,
-                          departure_time = departure_time,
+                          trip_date,
+                          departure_time,
                           mode = mode,
-                          max_trip_duration = max_trip_duration)
+                          max_trip_duration,
+                          verbose = FALSE)
 
 # expected behavior
 test_that("setup_r5 - expected behavior", {
@@ -52,65 +54,38 @@ test_that("travel_time_matrix - expected errors", {
                         destinations = points,
                         trip_date,
                         departure_time,
-                        direct_modes = direct_modes,
-                        transit_modes = transit_modes,
-                        max_street_time = max_street_time,
-                        max_trip_duration = max_trip_duration))
+                        mode,
+                        max_street_time,
+                        max_trip_duration))
   testthat::expect_error(
     travel_time_matrix( r5r_core,
                         origins = points,
                         destinations = 'a',
                         trip_date,
                         departure_time,
-                        direct_modes = direct_modes,
-                        transit_modes = transit_modes,
-                        max_street_time = max_street_time,
-                        max_trip_duration = max_trip_duration))
+                        mode,
+                        max_street_time,
+                        max_trip_duration))
 
   # invalid modes
   testthat::expect_error(
     travel_time_matrix( r5r_core = r5r_core,
                         origins = points_sf,
                         destinations = points_sf,
-                        trip_date = trip_date,
-                        departure_time = departure_time,
-                        direct_modes = "pogoball",
-                        transit_modes = transit_modes,
-                        max_street_time = max_street_time,
-                        max_trip_duration = max_trip_duration))
-
-  testthat::expect_error(
-    travel_time_matrix( r5r_core = r5r_core,
-                        origins = points_sf,
-                        destinations = points_sf,
-                        trip_date = trip_date,
-                        departure_time = departure_time,
-                        direct_modes = direct_modes,
-                        transit_modes = 'mothership',
-                        max_street_time = max_street_time,
-                        max_trip_duration = max_trip_duration))
-
-  # invalid max_trip_duration
-  testthat::expect_error(
-    travel_time_matrix( r5r_core,
-                        origins = points,
-                        destinations = points,
                         trip_date,
                         departure_time,
-                        direct_modes = direct_modes,
-                        transit_modes = transit_modes,
-                        max_street_time = 'a',
-                        max_trip_duration = max_trip_duration))
+                        mode = "pogoball",
+                        max_trip_duration))
 
-  testthat::expect_error(
-    travel_time_matrix( r5r_core,
-                        origins = points,
-                        destinations = points,
-                        trip_date,
-                        departure_time,
-                        direct_modes = direct_modes,
-                        transit_modes = transit_modes,
-                        max_street_time = max_street_time,
-                        max_trip_duration = 'a'))
+
+  # # invalid max_trip_duration
+  # testthat::expect_error(
+  #   travel_time_matrix( r5r_core,
+  #                       origins = points,
+  #                       destinations = points,
+  #                       trip_date,
+  #                       departure_time,
+  #                       mode,
+  #                       max_trip_duration = 'aaa'))
 
   })
