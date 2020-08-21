1fbb74f29c4e9cc6244cde0ec5efcbfd8d5110e8
rafapereirabr <rafa.pereira.br@gmail.com>
2020-08-21 13:22:52 -0300

    remove magrittr and dplyr dependencies bit

--- r-package/R/zzz.R
+++ r-package/R/zzz.R
@@ -5,13 +5,11 @@ utils::globalVariables(c(".", "%>%", ":=", "%like%", "%chin%"))
   requireNamespace("sf")
   requireNamespace("rJava")
   requireNamespace("data.table")
-  requireNamespace("dplyr")
   # set number of threads used in data.table to 100%
   options(datatable.optimize = Inf) # nocov
   data.table::setDTthreads(percent = 100) # nocov
 }
 
-#' @importFrom magrittr %>%
 #' @importFrom data.table := %between% fifelse %chin%
 # #' @importFrom methods  is signature
 NULL
