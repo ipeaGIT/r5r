1ed52f39ba11b0362c24938e488fd302d478fa09
rafapereirabr <rafa.pereira.br@gmail.com>
2020-08-24 12:56:05 -0300

    add  eval = FALSE to vignette

--- r-package/R/zzz.R
+++ r-package/R/zzz.R
@@ -19,7 +19,7 @@ NULL
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1") utils::globalVariables(
   c('is', 'duration', 'fromId', 'toId', 'option', 'option', '.SD', 'geometry',
-    'route', 'temp_duration', 'temp_route', 'route', 'temp_sign'))
+    'route', 'temp_duration', 'temp_route', 'route', 'temp_sign', '.I'))
 
 
 
