da4b787fe96aabd856c31653d23f97c575301ab5
Marcus Saraiva <marcus.saraiva@gmail.com>
2020-08-24 20:57:34 -0300

    Fixing <list> results in travel time matrix. Those are caused by empty dataframes returned from Java. Now, null values are returned when no destination can be reached from some origin.

--- r-package/tests_marcus/test_poa.R
+++ r-package/tests_marcus/test_poa.R
@@ -1,21 +1,21 @@
 library("r5r")
-library("ggplot2")
 library("tidyverse")
 
 # Start R5R core
-r5r_core <- setup_r5(system.file("extdata", package = "r5r"), verbose = FALSE)
+r5r_core <- setup_r5("/Users/marcussaraiva/Repos/data_r5r", verbose = FALSE)
 
 # Load points of interest
 points <- read.csv(system.file("extdata/poa_points_of_interest.csv", package = "r5r"))
 
+points_hex <- read.csv(system.file("extdata/poa_hexgrid.csv", package = "r5r"))
 
 # Configuring trip
 origin <- points[10,] # Farrapos train station
 destination <- points[12,] # Praia de Belas shopping mall
 
-trip_date_time <- lubridate::as_datetime("2019-03-20 14:00:00")
+trip_date_time <- lubridate::as_datetime("2019-05-20 14:00:00")
 
-max_walk_distance = Inf
+max_walk_distance = 1000
 max_trip_duration = 120L
 
 paths_df <- detailed_itineraries(r5r_core = r5r_core,
@@ -32,7 +32,10 @@ paths_df %>%
   facet_wrap(~option)
 
 ttm <- travel_time_matrix(r5r_core, points, points, mode = c("WALK", "BUS"), trip_date_time, max_walk_dist = 2000,
-                          max_trip_duration = 120L)
+                          max_trip_duration = 120L, verbose = FALSE)
+ttm <- travel_time_matrix(r5r_core, points_hex, points, mode = c("WALK", "BUS"), trip_date_time, max_walk_dist = 2000,
+                          max_trip_duration = 120L, verbose = FALSE)
+ttm
 
 ttm %>%
   left_join(points, by=c("toId"="id")) %>%
@@ -43,3 +46,4 @@ ttm %>%
   theme_minimal() +
   coord_map()
 
+ttm
