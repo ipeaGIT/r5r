b1bad9a2715c8802758d2e96361516f42bc2a808
rafapereirabr <rafa.pereira.br@gmail.com>
2020-08-26 20:18:32 -0300

    documentation replace r5 with r5r

--- r-package/R/street_network_to_sf.R
+++ r-package/R/street_network_to_sf.R
@@ -1,7 +1,7 @@
 #' Extract OpenStreetMap network in sf format from a network.dat built with setup_r5
 #'
 #'
-#' @param r5_core a rJava object, the output from 'r5r::setup_r5()'
+#' @param r5r_core a rJava object, the output from 'r5r::setup_r5()'
 #'
 #' @return A list with two components of a street network in sf format: vertices
 #'         (POINT) and edges (LINESTRING).
@@ -14,21 +14,21 @@
 #'
 #' # build transport network
 #' path <- system.file("extdata", package = "r5r")
-#' r5_core <- setup_r5(data_path = path)
+#' r5r_core <- setup_r5(data_path = path)
 #'
 #' # load origin/destination points
-#' street_net <- street_network_to_sf(r5_core)
+#' street_net <- street_network_to_sf(r5r_core)
 #' }
 #' @export
 
-street_network_to_sf <- function(r5_core) {
+street_network_to_sf <- function(r5r_core) {
 
   # check input
-  if(class(r5_core)[1] != "jobjRef"){
+  if(class(r5r_core)[1] != "jobjRef"){
   stop("Input must be an object of class 'jobjRef' built with 'r5r::setup_r5()'")}
 
   # Get street network from R5R core
-  network <- r5_core$getStreetNetwork()
+  network <- r5r_core$getStreetNetwork()
 
   # Convert vertices to SF (point)
   vertices_df <- jdx::convertToR(network$get(0L), array.order = "column-major")
