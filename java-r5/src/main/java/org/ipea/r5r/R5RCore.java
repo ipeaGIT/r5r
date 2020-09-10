71949640761ab8424b12b3d14e2c284fb0f1309a
Marcus Saraiva <marcus.saraiva@gmail.com>
2020-09-10 11:04:30 -0300

    Returning complete route information in getTransitNetwork()

--- java-r5/src/main/java/org/ipea/r5r/R5RCore.java
+++ java-r5/src/main/java/org/ipea/r5r/R5RCore.java
@@ -17,10 +17,13 @@ import com.conveyal.r5.kryo.KryoNetworkSerializer;
 import com.conveyal.r5.point_to_point.builder.PointToPointQuery;
 import com.conveyal.r5.streets.EdgeStore;
 import com.conveyal.r5.streets.VertexStore;
+import com.conveyal.r5.transit.RouteInfo;
+import com.conveyal.r5.transit.TransitLayer;
 import com.conveyal.r5.transit.TransportNetwork;
 import com.conveyal.r5.transit.TripPattern;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.LineString;
+import org.mapdb.Atomic;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
@@ -672,19 +675,45 @@ public class R5RCore {
         // Build transit network
 
         // routes and shape geometries
-        ArrayList<String> routeIdCol = new ArrayList<>();
-        ArrayList<String> routeGeometryCol = new ArrayList<>();
-
-        LinkedHashMap<String, Object> routesTable = new LinkedHashMap<>();
-        routesTable.put("routeId", routeIdCol);
-        routesTable.put("geometry", routeGeometryCol);
+        RDataFrame routesTable = new RDataFrame();
+        routesTable.addStringColumn("agency_id", "");
+        routesTable.addStringColumn("agency_name", "");
+        routesTable.addStringColumn("route_id", "");
+        routesTable.addStringColumn("long_name", "");
+        routesTable.addStringColumn("short_name", "");
+        routesTable.addStringColumn("mode", "");
+        routesTable.addStringColumn("geometry", "");
 
         for (TripPattern pattern : transportNetwork.transitLayer.tripPatterns) {
-            routeIdCol.add(pattern.routeId);
+            RouteInfo route = transportNetwork.transitLayer.routes.get(pattern.routeIndex);
+
+            routesTable.append();
+            routesTable.set("agency_id", route.agency_id);
+            routesTable.set("agency_name", route.agency_name);
+            routesTable.set("route_id", route.route_id);
+            routesTable.set("long_name", route.route_long_name);
+            routesTable.set("short_name", route.route_short_name);
+            routesTable.set("mode", TransitLayer.getTransitModes(route.route_type).toString());
+
             if (pattern.shape != null) {
-                routeGeometryCol.add(pattern.shape.toString());
+                routesTable.set("geometry", pattern.shape.toString());
             } else {
-                routeGeometryCol.add("");
+                // build geometry from stops
+                StringBuilder geometry = new StringBuilder();
+                for (int stopIndex : pattern.stops) {
+                    Coordinate coordinate = transportNetwork.transitLayer.getCoordinateForStopFixed(stopIndex);
+
+                    coordinate.x = coordinate.x / FIXED_FACTOR;
+                    coordinate.y = coordinate.y / FIXED_FACTOR;
+
+                    if (geometry.toString().equals("")) {
+                        geometry.append("LINESTRING (").append(coordinate.x).append(" ").append(coordinate.y);
+                    } else {
+                        geometry.append(", ").append(coordinate.x).append(" ").append(coordinate.y);
+                    }
+                }
+                geometry.append(")");
+                routesTable.set("geometry", geometry.toString());
             }
         }
 
@@ -697,20 +726,29 @@ public class R5RCore {
         stopsTable.addDoubleColumn("lon", -1.0);
         stopsTable.addBooleanColumn("linked_to_street", false);
 
+        LOG.info("Getting public transport stops from Transport Network");
+        LOG.info("{} stops were found in the network", transportNetwork.transitLayer.getStopCount());
+
         for (int stopIndex = 0; stopIndex < transportNetwork.transitLayer.getStopCount(); stopIndex++) {
+            LOG.info("Stop #{}", stopIndex);
+            LOG.info("Stop id: {}", transportNetwork.transitLayer.stopIdForIndex.get(stopIndex));
+
             stopsTable.append();
             stopsTable.set("stop_index", stopIndex);
             stopsTable.set("stop_id", transportNetwork.transitLayer.stopIdForIndex.get(stopIndex));
 
-            if (!transportNetwork.transitLayer.stopNames.isEmpty()) {
+            if (transportNetwork.transitLayer.stopNames != null) {
+                LOG.info("Stop name: {}", transportNetwork.transitLayer.stopNames.get(stopIndex));
                 stopsTable.set("stop_name", transportNetwork.transitLayer.stopNames.get(stopIndex));
             }
 
             Coordinate coordinate = transportNetwork.transitLayer.getCoordinateForStopFixed(stopIndex);
-            coordinate.x = coordinate.x / FIXED_FACTOR;
-            coordinate.y = coordinate.y / FIXED_FACTOR;
-            stopsTable.set("lat", coordinate.y);
-            stopsTable.set("lon", coordinate.x);
+            if (coordinate != null) {
+                Double lat = coordinate.y / FIXED_FACTOR;
+                Double lon = coordinate.x / FIXED_FACTOR;
+                stopsTable.set("lat", lat);
+                stopsTable.set("lon", lon);
+            }
 
             boolean linkedToStreet = (transportNetwork.transitLayer.streetVertexForStop.get(stopIndex) != -1);
             stopsTable.set("linked_to_street", linkedToStreet);
@@ -718,7 +756,7 @@ public class R5RCore {
 
         // Return a list of dataframes
         List<Object> transportNetworkList = new ArrayList<>();
-        transportNetworkList.add(routesTable);
+        transportNetworkList.add(routesTable.getDataFrame());
         transportNetworkList.add(stopsTable.getDataFrame());
 
         return transportNetworkList;
