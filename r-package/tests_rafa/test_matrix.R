d9508ee766c29de78b43aafcca294f24470f377f
Marcus Saraiva <marcus.saraiva@gmail.com>
2020-09-29 11:11:08 -0300

    Fixed bugs in time window travel time matrix calculation: - accept a single percentile as input - set default values to R5's defaults - travel time matrix function now accepts a single origin or single destination as inputs

--- r-package/tests_rafa/test_matrix.R
+++ r-package/tests_rafa/test_matrix.R
@@ -1,32 +1,37 @@
 library(r5r)
+devtools::load_all()
 
 # build transport network
 data_path <- system.file("extdata", package = "r5r")
 r5r_core <- setup_r5(data_path = data_path)
 
 # load origin/destination points
-points <- read.csv(system.file("extdata/poa_hexgrid.csv", package = "r5r"))[1:5,]
+points <- read.csv(system.file("extdata/poa_hexgrid.csv", package = "r5r"))
+poi <- read.csv(system.file("extdata/poa_points_of_interest.csv", package = "r5r"))
 
 mode <- c("WALK", "TRANSIT")
-max_walk_dist <- Inf
+max_walk_dist <- 1000
 max_trip_duration <- 120L
-departure_datetime <- as.POSIXct("13-03-2019 14:00:00",
-                                format = "%d-%m-%Y %H:%M:%S")
+departure_datetime <- lubridate::as_datetime("2019-03-20 14:00:00")
 
 
 
 
 # estimate travel time matrix
-travel_time_matrix(r5r_core,
-                         origins = points,
-                         destinations = points,
-                         mode,
-                         departure_datetime,
-                         max_walk_dist,
-                         max_trip_duration
-                          # , time_window= 60
-                          # , percentiles =  c(50, 50)
-                         )
+tictoc::tic()
+ttm <- travel_time_matrix(r5r_core,
+                          origins = points,
+                          destinations = points,
+                          mode = mode,
+                          departure_datetime = departure_datetime,
+                          max_walk_dist = max_walk_dist,
+                          max_trip_duration = max_trip_duration
+                          , time_window= 60
+                          # , percentiles = c(25, 50, 75, 100)
+)
+tictoc::toc()
+
+
 
 head(ttm)
 
