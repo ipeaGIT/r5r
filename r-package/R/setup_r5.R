8b664baa5d4287fc1329fe7b1f8e1525756776e6
rafapereirabr <rafa.pereira.br@gmail.com>
2021-03-08 16:54:49 -0300

    reformat error message

--- r-package/R/setup_r5.R
+++ r-package/R/setup_r5.R
@@ -43,12 +43,14 @@ setup_r5 <- function(data_path,
     rJava::.jinit()
     ver <- rJava::.jcall("java.lang.System","S","getProperty","java.version")
     ver <- as.numeric(gsub("\\..*","",ver))
-    if (ver != 11){stop("This package requires the Java SE Development Kit 11.
-                        Please update your Java installation. The jdk 11 can be
-                        downloaded from either:
-                         - openjdk: https://jdk.java.net/java-se-ri/11
-                         - oracle: https://www.oracle.com/java/technologies/javase-jdk11-downloads.html")}
-
+    if (ver < 11){
+      stop(
+        "This package requires the Java SE Development Kit 11.\n",
+        "Please update your Java installation. The jdk 11 can be downloaded from either:\n",
+        "  - openjdk: https://jdk.java.net/java-se-ri/11\n",
+        "  - oracle: https://www.oracle.com/java/technologies/javase-jdk11-downloads.html"
+      )
+    }
 
   # check directory input
   if (is.null(data_path)){ stop("Please provide data_path.")}
