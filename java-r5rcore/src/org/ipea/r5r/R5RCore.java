7e4e5a015bc0b6abeee22eb542d0eeb32ca4bf41
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-05-05 20:04:12 -0300

    Refactoring: function to create RegionalTaskRefactoring: moving excess code from R5RCore to other classes - Focus on StreetNetwork class, used by `street_network_to_sf()`

--- java-r5rcore/src/org/ipea/r5r/R5RCore.java
+++ java-r5rcore/src/org/ipea/r5r/R5RCore.java
@@ -747,54 +747,13 @@ public class R5RCore {
     }
 
     public List<Object> getStreetNetwork() {
-        // Build vertices return table
-        RDataFrame verticesTable = new RDataFrame();
-        verticesTable.addIntegerColumn("index", 0);
-        verticesTable.addDoubleColumn("lat", 0.0);
-        verticesTable.addDoubleColumn("lon", 0.0);
-        verticesTable.addBooleanColumn("park_and_ride", false);
-        verticesTable.addBooleanColumn("bike_sharing", false);
-
-        VertexStore vertices = transportNetwork.streetLayer.vertexStore;
-
-        VertexStore.Vertex vertexCursor = vertices.getCursor();
-        while (vertexCursor.advance()) {
-            verticesTable.append();
-            verticesTable.set("index", vertexCursor.index);
-            verticesTable.set("lat", vertexCursor.getLat());
-            verticesTable.set("lon", vertexCursor.getLon());
-            verticesTable.set("park_and_ride", vertexCursor.getFlag(VertexStore.VertexFlag.PARK_AND_RIDE));
-            verticesTable.set("bike_sharing", vertexCursor.getFlag(VertexStore.VertexFlag.BIKE_SHARING));
-        }
-
-        // Build edges return table
-        RDataFrame edgesTable = new RDataFrame();
-        edgesTable.addIntegerColumn("from_vertex", 0);
-        edgesTable.addIntegerColumn("to_vertex", 0);
-        edgesTable.addDoubleColumn("length", 0.0);
-        edgesTable.addBooleanColumn("walk", false);
-        edgesTable.addBooleanColumn("bicycle", false);
-        edgesTable.addBooleanColumn("car", false);
-        edgesTable.addStringColumn("geometry", "");
-
-        EdgeStore edges = transportNetwork.streetLayer.edgeStore;
-
-        EdgeStore.Edge edgeCursor = edges.getCursor();
-        while (edgeCursor.advance()) {
-            edgesTable.append();
-            edgesTable.set("from_vertex", edgeCursor.getFromVertex());
-            edgesTable.set("to_vertex", edgeCursor.getToVertex());
-            edgesTable.set("length", edgeCursor.getLengthM());
-            edgesTable.set("walk", edgeCursor.allowsStreetMode(StreetMode.WALK));
-            edgesTable.set("bicycle", edgeCursor.allowsStreetMode(StreetMode.BICYCLE));
-            edgesTable.set("car", edgeCursor.allowsStreetMode(StreetMode.CAR));
-            edgesTable.set("geometry", edgeCursor.getGeometry().toString());
-        }
+        // Convert R5's transport network to Simple Features objects
+        StreetNetwork streetNetwork = new StreetNetwork(this.transportNetwork);
 
         // Return a list of dataframes
         List<Object> transportNetworkList = new ArrayList<>();
-        transportNetworkList.add(verticesTable.getDataFrame());
-        transportNetworkList.add(edgesTable.getDataFrame());
+        transportNetworkList.add(streetNetwork.verticesTable.getDataFrame());
+        transportNetworkList.add(streetNetwork.edgesTable.getDataFrame());
 
         return transportNetworkList;
     }
