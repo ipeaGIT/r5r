8630842bc99d3f8e2170256c8ed9edd71e13356d
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-05-11 15:57:48 -0300

    Modified test_snap

--- r-package/tests_marcus/test_snap.R
+++ r-package/tests_marcus/test_snap.R
@@ -2,23 +2,23 @@
 options(java.parameters = "-Xmx16G")
 
 devtools::load_all(".")
-library(tidyverse)
 library(mapview)
-# system.file returns the directory with example data inside the r5r package
-# set data path to directory containing your own data if not using the examples
-data_path <- system.file("extdata/poa", package = "r5r")
 
+# initialize r5r
+data_path <- system.file("extdata/poa", package = "r5r")
 r5r_core <- setup_r5(data_path, verbose = FALSE)
 
+# get regular grid at resolution 8
 grid_df <- r5r_core$getGrid(8L)
 grid_df <- jdx::convertToR(grid_df)
+mv1 <- mapview(grid_df, xcol="lon", ycol="lat", crs=4326)
 
-grid_df %>% mapview(xcol="lon", ycol="lat", crs=4326)
-
+# snap grid to street network
 snap_df <- r5r_core$findSnapPoints(grid_df$id, grid_df$lat, grid_df$lon, "CAR")
 snap_df <- jdx::convertToR(snap_df)
-snap_df %>% mapview(xcol="snap_lon", ycol="snap_lat", zcol="found", crs=4326)
+mv2 <- mapview(snap_df, xcol="snap_lon", ycol="snap_lat", zcol="found", crs=4326)
 
+sync(mv1, mv2)
 
 
 
