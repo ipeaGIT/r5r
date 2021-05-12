04c44e752e018e2a3e61ec56655399fa3fe9f7ff
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-05-12 15:33:09 -0300

    Modified test_snap

--- r-package/tests_marcus/test_snap.R
+++ r-package/tests_marcus/test_snap.R
@@ -1,8 +1,4 @@
-##### Reprex 1 - Parallel Computing #####
-options(java.parameters = "-Xmx16G")
-
-devtools::load_all(".")
-library(mapview)
+library(r5r)
 
 # initialize r5r
 data_path <- system.file("extdata/poa", package = "r5r")
@@ -11,14 +7,15 @@ r5r_core <- setup_r5(data_path, verbose = FALSE)
 # get regular grid at resolution 8
 grid_df <- r5r_core$getGrid(8L)
 grid_df <- jdx::convertToR(grid_df)
-mv1 <- mapview(grid_df, xcol="lon", ycol="lat", crs=4326)
 
 # snap grid to street network
 snap_df <- r5r_core$findSnapPoints(grid_df$id, grid_df$lat, grid_df$lon, "CAR")
 snap_df <- jdx::convertToR(snap_df)
-mv2 <- mapview(snap_df, xcol="snap_lon", ycol="snap_lat", zcol="found", crs=4326)
 
-sync(mv1, mv2)
+mv1 <- mapview::mapview(snap_df, xcol="lon", ycol="lat", crs=4326)
+mv2 <- mapview::mapview(snap_df, xcol="snap_lon", ycol="snap_lat", zcol="found", crs=4326)
+
+leafsync::sync(mv1, mv2)
 
 
 
