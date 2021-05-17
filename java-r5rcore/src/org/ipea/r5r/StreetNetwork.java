ba0816f0e05a157c209466c9cde1195f183d6b41
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-05-17 16:46:16 -0300

    Modified the StreetNetwork class, used by `street_network_to_sf()`, to remove extra edges and vertices that represent only transit stops. Those edges produced weird artefacts around the sample datasets provided when using `street_network_to_sf()`.

--- java-r5rcore/src/org/ipea/r5r/StreetNetwork.java
+++ java-r5rcore/src/org/ipea/r5r/StreetNetwork.java
@@ -5,14 +5,23 @@ import com.conveyal.r5.streets.EdgeStore;
 import com.conveyal.r5.streets.VertexStore;
 import com.conveyal.r5.transit.TransportNetwork;
 
+import java.util.HashSet;
+
 public class StreetNetwork {
 
     public RDataFrame verticesTable;
     public RDataFrame edgesTable;
 
+
+    // R5 creates additional vertices on the street network to represent transit stops, so this HashSet is used to
+    // uniquely identify street vertices that are connected to the original street edges that came from OSM
+    private final HashSet<Integer> verticesSet;
+
     public StreetNetwork(TransportNetwork transportNetwork) {
-        buildVerticesTable(transportNetwork);
+        verticesSet = new HashSet<Integer>();
+
         buildEdgesTable(transportNetwork);
+        buildVerticesTable(transportNetwork);
     }
 
     private void buildEdgesTable(TransportNetwork transportNetwork) {
@@ -30,14 +39,23 @@ public class StreetNetwork {
 
         EdgeStore.Edge edgeCursor = edges.getCursor();
         while (edgeCursor.advance()) {
-            edgesTable.append();
-            edgesTable.set("from_vertex", edgeCursor.getFromVertex());
-            edgesTable.set("to_vertex", edgeCursor.getToVertex());
-            edgesTable.set("length", edgeCursor.getLengthM());
-            edgesTable.set("walk", edgeCursor.allowsStreetMode(StreetMode.WALK));
-            edgesTable.set("bicycle", edgeCursor.allowsStreetMode(StreetMode.BICYCLE));
-            edgesTable.set("car", edgeCursor.allowsStreetMode(StreetMode.CAR));
-            edgesTable.set("geometry", edgeCursor.getGeometry().toString());
+            // edges that have the LINK flag are created by R5 specifically to link the OSM street network to bus stops,
+            // so such edges are removed from the return data.frame
+            if (!edgeCursor.getFlag(EdgeStore.EdgeFlag.LINK)) {
+                edgesTable.append();
+                edgesTable.set("from_vertex", edgeCursor.getFromVertex());
+                edgesTable.set("to_vertex", edgeCursor.getToVertex());
+                edgesTable.set("length", edgeCursor.getLengthM());
+                edgesTable.set("walk", edgeCursor.allowsStreetMode(StreetMode.WALK));
+                edgesTable.set("bicycle", edgeCursor.allowsStreetMode(StreetMode.BICYCLE));
+                edgesTable.set("car", edgeCursor.allowsStreetMode(StreetMode.CAR));
+                edgesTable.set("geometry", edgeCursor.getGeometry().toString());
+
+                // if the edge is originally from OSM, add its from/to vertices to the vertices HashSet, so they can
+                // be returned later in the vertices data.frame
+                verticesSet.add(edgeCursor.getFromVertex());
+                verticesSet.add(edgeCursor.getToVertex());
+            }
         }
     }
 
@@ -51,15 +69,18 @@ public class StreetNetwork {
         verticesTable.addBooleanColumn("bike_sharing", false);
 
         VertexStore vertices = transportNetwork.streetLayer.vertexStore;
-
         VertexStore.Vertex vertexCursor = vertices.getCursor();
-        while (vertexCursor.advance()) {
+
+        // iterate over vertices previously identified in the edges table, and add them to the return data.frame
+        verticesSet.forEach(v -> {
+            vertexCursor.seek(v);
+
             verticesTable.append();
             verticesTable.set("index", vertexCursor.index);
             verticesTable.set("lat", vertexCursor.getLat());
             verticesTable.set("lon", vertexCursor.getLon());
             verticesTable.set("park_and_ride", vertexCursor.getFlag(VertexStore.VertexFlag.PARK_AND_RIDE));
             verticesTable.set("bike_sharing", vertexCursor.getFlag(VertexStore.VertexFlag.BIKE_SHARING));
-        }
+        });
     }
 }
--- r-package/tests_marcus/test_snap.R
+++ r-package/tests_marcus/test_snap.R
@@ -1,6 +1,8 @@
 # library(tidyverse)
 library(r5r)
-# library(sf)
+library(sf)
+library(tidyverse)
+library(mapview)
 
 # initialize r5r
 data_path <- system.file("extdata/poa", package = "r5r")
@@ -12,12 +14,16 @@ grid_df <- jdx::convertToR(grid_df)
 grid_df$geometry <- st_as_sfc(grid_df$geometry)
 grid_df <- st_as_sf(grid_df, crs = 4326)
 
-grid_df %>% mapview::mapview()
+grid_df %>% mutate(id = as.integer(id)) %>% mapview::mapview(zcol = "id")
+  mapview::mapview(snap_df %>% filter(point_id=="4"), xcol="lon", ycol="lat") +
+  mapview::mapview(snap_df %>% filter(point_id=="4"), xcol="snap_lon", ycol="snap_lat")
 
 
 # snap grid to street network
 snap_df <- r5r_core$findSnapPoints(grid_df$id, grid_df$lat, grid_df$lon, "WALK")
 snap_df <- jdx::convertToR(snap_df)
+snap_df <- snap_df %>%
+  filter(found == TRUE)
 
 mapview::mapview(snap_df, xcol="lon", ycol="lat", crs=4326) +
 mapview::mapview(snap_df, xcol="snap_lon", ycol="snap_lat", zcol="found", crs=4326)
@@ -25,5 +31,28 @@ mapview::mapview(snap_df, xcol="snap_lon", ycol="snap_lat", zcol="found", crs=43
 leafsync::sync(mv1, mv2)
 
 
+original <- snap_df %>% select(id = point_id, lat, lon)
+snapped <- snap_df %>% select(id = point_id, lat = snap_lat, lon = snap_lon)
+
+ttm_orig <- travel_time_matrix(r5r_core, origins = original, destinations = original,
+                          verbose = FALSE)
+
+ttm_snap <- travel_time_matrix(r5r_core, origins = snapped, destinations = snapped,
+                          verbose = FALSE)
+
+ttm_join <- left_join(ttm_orig, ttm_snap, by=c("fromId", "toId"),
+                      suffix = c("_or", "_sn"))
+
+street_net <- street_network_to_sf(r5r_core)
+
+street_net$vertices %>% mapview() +
+street_net$edges %>% mapview()
+
+hex <- read_csv(system.file("extdata/poa", "poa_hexgrid.csv", package = "r5r"))
+
+hex %>%
+  mapview(xcol="lon", ycol="lat", crs=4326)
+
+min(hex$lat)
 
 
