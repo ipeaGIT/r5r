c8d8e52c57d0deae52f4423076b2965a317f27a3
rafapereirabr <rafa.pereira.br@gmail.com>
2021-07-01 11:40:29 -0300

    fix elevation test

--- r-package/tests/testthat/test-elevation_utils.R
+++ r-package/tests/testthat/test-elevation_utils.R
@@ -16,7 +16,7 @@ test_that("tobler_hiking", {
 
   expect_error( tobler_hiking('bananas') )
   expect_error( tobler_hiking() )
-  expect_identical( round(tobler_hiking(1)), 33)
+  expect_identical( round( r5r:::tobler_hiking(1)), 33)
 
 })
 
@@ -26,11 +26,11 @@ test_that("tobler_hiking", {
 
 test_that("apply_elevation", {
 
-  expect_silent( apply_elevation(r5r_core, raster_poa) )
-  expect_silent( apply_elevation(r5r_core, c(raster_poa,raster_poa)) )
+  expect_silent( r5r:::apply_elevation(r5r_core, raster_poa) )
+  expect_silent( r5r:::apply_elevation(r5r_core, c(raster_poa,raster_poa)) )
 
-  expect_error( apply_elevation('bananas', raster_poa) )
-  expect_error( apply_elevation(r5r_core, 'bananas') )
+  expect_error( r5r:::apply_elevation('bananas', raster_poa) )
+  expect_error( r5r:::apply_elevation(r5r_core, 'bananas') )
 
   })
 
