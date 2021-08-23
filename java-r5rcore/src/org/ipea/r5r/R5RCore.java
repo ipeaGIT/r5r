4f8c47893e048681acd9b6a3769cb245cc2eeaf6
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-08-23 13:55:14 -0300

    Added `defaultBuildConfig()` function to r5r core jar. Issue #187

--- java-r5rcore/src/org/ipea/r5r/R5RCore.java
+++ java-r5rcore/src/org/ipea/r5r/R5RCore.java
@@ -3,6 +3,7 @@ package org.ipea.r5r;
 import com.conveyal.gtfs.model.Service;
 import com.conveyal.r5.analyst.Grid;
 import com.conveyal.r5.analyst.decay.*;
+import com.conveyal.r5.point_to_point.builder.TNBuilderConfig;
 import com.conveyal.r5.streets.EdgeStore;
 import com.conveyal.r5.streets.EdgeTraversalTimes;
 import com.conveyal.r5.transit.TransferFinder;
@@ -628,8 +629,12 @@ public class R5RCore {
             return null;
         }
 
+    }
 
+    public String defaultBuildConfig() {
+        TNBuilderConfig builderConfig = TNBuilderConfig.defaultConfig();
 
+        return builderConfig.toString();
     }
 
 }
