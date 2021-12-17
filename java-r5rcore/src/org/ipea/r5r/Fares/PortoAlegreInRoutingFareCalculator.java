6eeba4ec03eaa95c140146ed67c73e03d11f8849
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-12-17 15:21:46 -0300

    Finished Porto Alegre fare calculator

--- java-r5rcore/src/org/ipea/r5r/Fares/PortoAlegreInRoutingFareCalculator.java
+++ java-r5rcore/src/org/ipea/r5r/Fares/PortoAlegreInRoutingFareCalculator.java
@@ -9,9 +9,24 @@ import gnu.trove.iterator.TIntIterator;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
 
+
+class IntegratedFare {
+    public int fare;
+    public boolean usedDiscount;
+
+    IntegratedFare(int fare, boolean discount) {
+        this.fare = fare;
+        this.usedDiscount = discount;
+    }
+}
+
 public class PortoAlegreInRoutingFareCalculator extends InRoutingFareCalculator {
 
-    public int fare = 480;
+    public static int fareEptcFull = 480;
+    public static int getFareEptcDiscount = 240;
+    public static int fareTrensurb = 450;
+    public static int fareTrainAfterBus = 837 - fareEptcFull;
+    public static int fareBusAfterTrain = 837 - fareTrensurb;
 
     @Override
     public FareBounds calculateFare(McRaptorSuboptimalPathProfileRouter.McRaptorState state, int maxClockTime) {
@@ -27,32 +42,65 @@ public class PortoAlegreInRoutingFareCalculator extends InRoutingFareCalculator 
 
         patterns.reverse();
 
-        String previousRouteId = "";
+        RouteInfo previousRoute = null;
         boolean discountAlreadyApplied = false;
+
         for (TIntIterator patternIt = patterns.iterator(); patternIt.hasNext();) {
             int pattern = patternIt.next();
 
             RouteInfo ri = transitLayer.routes.get(transitLayer.tripPatterns.get(pattern).routeIndex);
 
-            if (previousRouteId.equals("") || discountAlreadyApplied) {
+            if (previousRoute == null || discountAlreadyApplied) {
                 // first public transport leg or discount already applied... full ticket price
-                fareForState += fare;
+                fareForState += getFullFareForRoute(ri);
             } else {
                 // test if discount for subsequent legs is applicable
-                if (previousRouteId.equals(ri.route_id)) {
-                    // if same bus, then full price
-                    fareForState += fare;
-                } else {
-                    // if different bus, then half price
-                    fareForState += fare / 2;
-                    discountAlreadyApplied = true;
-                }
+                IntegratedFare integratedFare = getIntegrationFare(previousRoute, ri);
+
+                fareForState += integratedFare.fare;
+                if (integratedFare.usedDiscount) discountAlreadyApplied = true;
             }
 
-            previousRouteId = ri.route_id;
+            previousRoute = ri;
         }
 
         return new FareBounds(fareForState, new TransferAllowance());
+    }
+
+    private int getFullFareForRoute(RouteInfo ri) {
+        if (ri.agency_id.equals("TRENS")) {
+            return fareTrensurb;
+        } else {
+            return fareEptcFull;
+        }
+    }
+
+    private IntegratedFare getIntegrationFare(RouteInfo firstRoute, RouteInfo secondRoute) {
+        String firstAgency = firstRoute.agency_id;
+        String secondAgency = secondRoute.agency_id;
+
+        // transfers between metro services and aeromovel are free
+        if (firstAgency.equals("TRENS") && secondAgency.equals("TRENS")) {
+            return new IntegratedFare(0, false);
+        }
+
+        // metro and train transfers have a small discount
+        if ( firstAgency.equals("TRENS") && secondAgency.equals("EPTC") ) {
+            return new IntegratedFare(fareBusAfterTrain, true); // total 837, first leg has cost 450
+        }
+
+        if ( firstAgency.equals("EPTC") && secondAgency.equals("TRENS")) {
+            return new IntegratedFare(fareTrainAfterBus, true); // total 837, first leg has cost 480
+        }
+
+        // transfer between bus services (EPTC -> EPTC)
+        if (firstRoute.route_id.equals(secondRoute.route_id)) {
+            // transfers between buses in the same route have full price
+            return new IntegratedFare(fareEptcFull, false);
+        } else {
+            // transfers between buses in different routes have a 50% discount
+            return new IntegratedFare(getFareEptcDiscount, true);
+        }
     }
 
     @Override
@@ -60,3 +108,4 @@ public class PortoAlegreInRoutingFareCalculator extends InRoutingFareCalculator 
         return "porto-alegre";
     }
 }
+
