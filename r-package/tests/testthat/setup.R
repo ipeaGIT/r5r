f95c2dd7d1b2b9563c61a10acbcdccfc07d013ce
Daniel Herszenhut <dhersz@gmail.com>
2022-05-05 17:56:40 -0300

    added test setup to reduce tests processing time

---
+++ r-package/tests/testthat/setup.R
@@ -0,0 +1,10 @@
+# testthat::skip_on_cran()
+
+options(java.parameters = '-Xmx2G')
+
+if (Sys.getenv("NOT_CRAN") != "false") {
+  data_path <- system.file("extdata/poa", package = "r5r")
+  r5r_core <- setup_r5(data_path, verbose = FALSE)
+  points <- read.csv(file.path(data_path, "poa_hexgrid.csv"))
+}
+
