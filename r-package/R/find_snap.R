9c780810585b9a69afd1dd88fd2bea62d45abd3c
Daniel Herszenhut <dhersz@gmail.com>
2022-05-11 17:30:37 -0300

    changes to find_snap() documentation

--- r-package/R/find_snap.R
+++ r-package/R/find_snap.R
@@ -1,11 +1,16 @@
 #' Find snapped locations of input points on street network
 #'
-#' @description R5 tries to snap origin and destination points to the street
-#' network in two rounds. First, it uses a search radius of 300 meters. If the
-#' first round is unsuccessful, then R5 expands the search radius to 1.6 km.
-#' Points that aren't linked to the street network after those two rounds are
-#' returned with `NA` coordinates and `found = FALSE`. Please note that the
-#' location of the snapped points depends on the transport mode set by the user.
+#' Finds the snapped location of points on `R5` network. Snapping is a very
+#' important step of the routing process, which is when the origins and
+#' destinations specified by the user are actually positioned on the network
+#' created by `R5`. The snapping process in `R5` is composed of two rounds.
+#' First, it tries to snap the points within a radius of 300 meters from
+#' themselves. If the first round is unsuccessful, then `R5` expands the search
+#' radius to 1.6 km. If yet again it is unsuccessful, then the unsnapped points
+#' won't be used during the routing process. The snapped location of each point
+#' depends on the transport mode set by the user, because some network edges
+#' are not available to specific modes (e.g. a pedestrian-only street cannot be
+#' used to snap car trips).
 #'
 #' @template r5r_core
 #' @param points Either a `POINT sf` object with WGS84 CRS, or a `data.frame`
@@ -13,48 +18,36 @@
 #' @param mode A string. Which mode to consider when trying to snap the points
 #' to the network. Defaults to `WALK`, also allows `BICYCLE` and `CAR`.
 #'
-#' @return A data.table with the original points as well as their respective
-#'         snapped coordinates on the street network and the Euclidean distance
-#'         between original points and their respective snapped location. Points
-#'         that could not be snapped show `NA` coordinates and `found = FALSE`.
+#' @return A `data.table` with the original points, their respective
+#' snapped coordinates on the street network and the Euclidean distance between
+#' the original points and their snapped location. Points that could not
+#' be snapped show `NA` coordinates and `found = FALSE`.
 #'
 #' @family network functions
 #'
 #' @examplesIf interactive()
 #' library(r5r)
 #'
-#' # build transport network
-#' path <- system.file("extdata/spo", package = "r5r")
+#' path <- system.file("extdata/poa", package = "r5r")
 #' r5r_core <- setup_r5(data_path = path, temp_dir = TRUE)
-#'
-#' # load origin/destination points
 #' points <- read.csv(file.path(path, "spo_hexgrid.csv"))
 #'
-#' # find where origin or destination points are snapped
-#' snap_df <- find_snap(r5r_core,
-#'                      points = points,
-#'                      mode = 'CAR')
+#' snap_df <- find_snap(r5r_core, points, mode = "WALK")
 #'
 #' stop_r5(r5r_core)
 #' @export
-find_snap <- function(r5r_core,
-                      points,
-                      mode = "WALK") {
-
-  # check inputs ------------------------------------------------------------
-
-  # r5r_core
+find_snap <- function(r5r_core, points, mode = "WALK") {
   checkmate::assert_class(r5r_core, "jobjRef")
 
-  # modes
-  if (!(mode %in% c('WALK','BICYCLE','CAR'))) {
-    stop(paste0(mode, " is not a valid 'mode'.\nPlease use one of the following: WALK, BICYCLE, CAR"))
-  }
+  mode_options <- c("WALK", "BICYCLE", "CAR")
+  checkmate::assert(
+    checkmate::check_string(mode),
+    checkmate::check_names(mode, subset.of = mode_options),
+    combine = "and"
+  )
 
-  # origins and destinations
-  points  <- assert_points_input(points, "points")
+  points <- assert_points_input(points, "points")
 
-  # snap points to street network
   snap_df <- r5r_core$findSnapPoints(points$id, points$lat, points$lon, mode)
   snap_df <- java_to_dt(snap_df)
 
