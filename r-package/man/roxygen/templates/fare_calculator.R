2fe31461f747b90970c336066a8e2e7a9557cfca
Daniel Herszenhut <dhersz@gmail.com>
2022-05-17 15:15:24 -0300

    changed accessibility docs and templates to agreed on style

--- r-package/man/roxygen/templates/fare_calculator.R
+++ r-package/man/roxygen/templates/fare_calculator.R
@@ -1,4 +1,4 @@
 #' @param fare_calculator A fare calculator object, following the convention
-#' set in [setup_fare_calculator()]. This object describes how transit fares
-#' should be calculated. Please see the fare calculator vignette to understand
-#' how this object is structured.
+#'   set in [setup_fare_calculator()]. This object describes how transit fares
+#'   should be calculated. Please see the fare calculator vignette to
+#'   understand how this object is structured.
