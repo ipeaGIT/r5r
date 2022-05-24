aa63a11411ce0d8136ea19bf69216add6566a0e8
rafapereirabr <rafa.pereira.br@gmail.com>
2022-05-24 18:38:40 -0300

    fixed typo from MANETTI to MINETTI

--- r-package/R/setup_r5.R
+++ r-package/R/setup_r5.R
@@ -20,7 +20,7 @@
 #' temporary directory. Defaults to `FALSE`.
 #' @param elevation A string. The name of the impedance function to be used to
 #' calculate impedance for walking and cycling based on street slopes.
-#' Available options include `TOBLER` (Default) and `MANETTI`, or `NONE` to
+#' Available options include `TOBLER` (Default) and `MINETTI`, or `NONE` to
 #' ignore elevation. R5 loads elevation data from `.tif` files saved inside the
 #' `data_path` directory.
 #' @param overwrite A logical. Whether to overwrite an existing `network.dat`
