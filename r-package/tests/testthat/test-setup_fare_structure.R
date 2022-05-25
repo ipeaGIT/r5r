477f2e94d2b2e6e8eb16093c3f00ed7f1e6acee0
Daniel Herszenhut <dhersz@gmail.com>
2022-05-25 18:30:15 -0300

    small fix to setup_fare_structure() test

--- r-package/tests/testthat/test-setup_fare_structure.R
+++ r-package/tests/testthat/test-setup_fare_structure.R
@@ -67,6 +67,7 @@ test_that("outputs a list with correct elements", {
       "fares_per_mode",
       "fares_per_transfer",
       "fares_per_route",
+      "transfer_time_allowance_seconds",
       "debug_settings"
     )
   )
