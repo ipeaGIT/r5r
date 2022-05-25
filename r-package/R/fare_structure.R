b35830b78c645544fa3451c1d152d761e72984c2
Daniel Herszenhut <dhersz@gmail.com>
2022-05-25 13:20:35 -0300

    properly using debug_path and debug_info in setup_fare_structure()

--- r-package/R/fare_structure.R
+++ r-package/R/fare_structure.R
@@ -9,40 +9,42 @@
 #'
 #' @template r5r_core
 #' @param base_fare A numeric. A base value used to populate the fare
-#' structure.
+#'   structure.
 #' @param by A string. Describes how `fare_type`s (a classification we created
-#' to assign fares to different routes) are distributed among routes. Possible
-#' values are `MODE`, `AGENCY` and `GENERIC`. `MODE` is used when the mode is
-#' what determines the price of a route (e.g. if all the buses of a given city
-#' cost $5). `AGENCY` is used when the agency that operates each route is what
-#' determines its price (i.e. when two different routes/modes operated by a
-#' single agency cost the same; note that you can also use `AGENCY_NAME`, if
-#' the agency_ids listed in your GTFS cannot be easily interpreted). `GENERIC`
-#' is used when all the routes cost the same. Please note that this
-#' classification can later be edited to better suit your needs (when, for
-#' example, two types of buses cost the same, but one offers discounts after
-#' riding the subway and the other one doesn't), but this parameter may save
-#' you some work.
+#'   to assign fares to different routes) are distributed among routes.
+#'   Possible values are `MODE`, `AGENCY` and `GENERIC`. `MODE` is used when
+#'   the mode is what determines the price of a route (e.g. if all the buses of
+#'   a given city cost $5). `AGENCY` is used when the agency that operates each
+#'   route is what determines its price (i.e. when two different routes/modes
+#'   operated by a single agency cost the same; note that you can also use
+#'   `AGENCY_NAME`, if the agency_ids listed in your GTFS cannot be easily
+#'   interpreted). `GENERIC` is used when all the routes cost the same. Please
+#'   note that this classification can later be edited to better suit your
+#'   needs (when, for example, two types of buses cost the same, but one offers
+#'   discounts after riding the subway and the other one doesn't), but this
+#'   parameter may save you some work.
 #' @param debug_path Either a path to a `.csv` file or `NULL`. When `NULL` (the
-#' default), fare debugging capabilities are disabled - i.e. there's no way to
-#' check if the fare calculation is correct. When a path is provided, `r5r`
-#' saves different itineraries and their respective fares to the specified
-#' file. How each itinerary is described is controlled by `debug_info`.
+#'   default), fare debugging capabilities are disabled - i.e. there's no way
+#'   to check if the fare calculation is correct. When a path is provided,
+#'   `r5r` saves different itineraries and their respective fares to the
+#'   specified file. How each itinerary is described is controlled by
+#'   `debug_info`.
 #' @param debug_info Either a string (when `debug_path` is a path) or `NULL`
-#' (the default). Doesn't have any effect if `debug_path` is `NULL`. When a
-#' string, accepts the values `MODE`, `ROUTE` and `MODE_ROUTE`. These values
-#' dictates how itinerary information is written to the output. Let's suppose
-#' we have an itinerary composed by two transit legs: first a subway leg whose
-#' route_id is 001, and then a bus legs whose route_id is 007. If `debug_info`
-#' is `MODE`, then this itinerary will be described as `SUBWAY|BUS`. If
-#' `ROUTE`, as `001|007`. If `MODE_ROUTE`, as `SUBWAY 001|BUS 007`. Please note
-#' that the final debug information will contain not only the itineraries that
-#' were in fact used in the itineraries returned in [travel_time_matrix()],
-#' [accessibility()] and [pareto_frontier()], but all the itineraries that `R5`
-#' checked when calculating the routes. This imposes a performance penalty when
-#' tracking debug information (but has the positive effect of returning a
-#' larger sample of itineraries, which might help finding some implementation
-#' issues on the fare structure).
+#'   (the default). Doesn't have any effect if `debug_path` is `NULL`. When a
+#'   string, accepts the values `MODE`, `ROUTE` and `MODE_ROUTE`. These values
+#'   dictates how itinerary information is written to the output. Let's suppose
+#'   we have an itinerary composed by two transit legs: first a subway leg
+#'   whose route_id is 001, and then a bus legs whose route_id is 007. If
+#'   `debug_info` is `MODE`, then this itinerary will be described as
+#'   `SUBWAY|BUS`. If `ROUTE`, as `001|007`. If `MODE_ROUTE`, as `SUBWAY
+#'   001|BUS 007`. Please note that the final debug information will contain
+#'   not only the itineraries that were in fact used in the itineraries
+#'   returned in [travel_time_matrix()], [accessibility()] and
+#'   [pareto_frontier()], but all the itineraries that `R5` checked when
+#'   calculating the routes. This imposes a performance penalty when tracking
+#'   debug information (but has the positive effect of returning a larger
+#'   sample of itineraries, which might help finding some implementation issues
+#'   on the fare structure).
 #'
 #' @return A fare structure object.
 #'
@@ -71,12 +73,12 @@
 #'
 #' @export
 setup_fare_structure <- function(r5r_core,
-                                  base_fare,
-                                  by = "MODE",
-                                  debug_path = NULL,
-                                  debug_info = NULL) {
+                                 base_fare,
+                                 by = "MODE",
+                                 debug_path = NULL,
+                                 debug_info = NULL) {
   checkmate::assert_class(r5r_core, "jobjRef")
-  checkmate::assert_numeric(base_fare, lower = 0.0)
+  checkmate::assert_numeric(base_fare, lower = 0, len = 1, any.missing = FALSE)
 
   by_options <- c("MODE", "AGENCY_ID", "AGENCY_NAME", "GENERIC")
   by <- toupper(by)
@@ -86,24 +88,44 @@ setup_fare_structure <- function(r5r_core,
     combine = "and"
   )
 
-  # call r5r_core method ----------------------------------------------------
+  checkmate::assert_string(debug_path, pattern = "\\.csv$", null.ok = TRUE)
+
+  if (is.null(debug_path) && !is.null(debug_info)) {
+    stop("Please specify a file to write debug info to with 'debug_path'.")
+  } else if (!is.null(debug_path) && is.null(debug_info)) {
+    debug_info <- "ROUTE"
+  }
+
+  debug_info_options <- c("MODE", "ROUTE", "MODE_ROUTE")
+  debug_info <- toupper(debug_info)
+  checkmate::assert(
+    checkmate::check_string(debug_info),
+    checkmate::check_names(debug_info, subset.of = debug_info_options),
+    combine = "and"
+  )
+
+  # r5r_core method to build fare structure returns a json
 
   f_struct <- r5r_core$buildFareStructure(rJava::.jfloat(base_fare), by)
-
   json_string <- f_struct$toJson()
+
   fare_structure <- jsonlite::parse_json(json_string, simplifyVector = TRUE)
 
   # Inf values are not supported by Java, so we use -1 to represent them
-  if (fare_structure$fare_cap <= 0) {
-    fare_structure$fare_cap <- Inf
+
+  if (fare_structure$fare_cap <= 0) fare_structure$fare_cap <- Inf
+
+  if (!is.null(debug_path)) {
+    debug <- list(
+      output_file = debug_path,
+      trip_info = debug_info
+    )
+  } else {
+    debug <- list(
+      output_file = "",
+      trip_info = "MODE"
+    )
   }
-
-  # attach debug settings
-  debug <- list(
-    output_file = "",
-    trip_info = "MODE"
-  )
-
   fare_structure$debug_settings <- debug
 
   data.table::setDT(fare_structure$fares_per_mode)
