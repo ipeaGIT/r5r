596582ca7291ebdcaa0a3df39515921e142228bb
Daniel Herszenhut <dhersz@gmail.com>
2022-06-03 17:09:46 -0300

    more set_verbose() tests

--- r-package/tests/testthat/test-set_verbose.R
+++ r-package/tests/testthat/test-set_verbose.R
@@ -3,6 +3,14 @@
 
 testthat::skip_on_cran()
 
+departure_datetime <- as.POSIXct(
+  "13-05-2019 14:00:00",
+  format = "%d-%m-%Y %H:%M:%S"
+)
+fare_structure <- read_fare_structure(
+  system.file("extdata/poa/fares/fares_poa.zip", package = "r5r")
+)
+
 tester <- function(verbose) set_verbose(r5r_core, verbose)
 
 test_that("input is correct", {
@@ -12,19 +20,54 @@ test_that("input is correct", {
 })
 
 test_that("verbose argument works in routing functions", {
-  verbose_messages <- capture.output(
-    res <- travel_time_matrix(r5r_core, pois[1], pois[1], verbose = TRUE),
-    type = "message"
-  )
-  expect_true(
-    any(grepl("(\\[main\\] INFO)|(\\[main\\] DEBUG)", verbose_messages))
-  )
+  assert_function <- function(f) {
+    expr <- if (identical(f, travel_time_matrix)) {
+      "f(r5r_core, pois[1:5], pois[1:5])"
+    } else if (identical(f, expanded_travel_time_matrix)) {
+      "f(r5r_core, pois[1:5], pois[1:5])"
+    } else if (identical(f, detailed_itineraries)) {
+      "f(r5r_core, pois[1:5], pois[5:1])"
+    } else if (identical(f, pareto_frontier)) {
+      "f(
+        r5r_core,
+        pois[1:5],
+        pois[1:5],
+        departure_datetime = departure_datetime,
+        fare_structure = fare_structure,
+        monetary_cost_cutoffs = c(0, 5, 10),
+        max_trip_duration = 60
+      )"
+    } else if (identical(f, accessibility)) {
+      "f(
+        r5r_core,
+        points[1:5],
+        points[1:5],
+        opportunities_colnames = \"schools\",
+        cutoffs = 60
+      )"
+    }
 
-  non_verbose_messages <- capture.output(
-    res <- travel_time_matrix(r5r_core, pois[1], pois[1], verbose = FALSE),
-    type = "message"
-  )
-  expect_false(
-    any(grepl("(\\[main\\] INFO)|(\\[main\\] DEBUG)", non_verbose_messages))
-  )
+    verbose_expr <- sub("\\)$", ", verbose = TRUE\\)", expr)
+    non_verbose_expr <- sub("\\)$", ", verbose = FALSE\\)", expr)
+
+    info_regex <- "(\\[.*\\] INFO)|(\\[.*\\] DEBUG)|(\\[.*\\] WARN)"
+
+    verbose_messages <- capture.output(
+      res <- eval(parse(text = verbose_expr)),
+      type = "message"
+    )
+    expect_true(any(grepl(info_regex, verbose_messages)))
+
+    non_verbose_messages <- capture.output(
+      res <- eval(parse(text = non_verbose_expr)),
+      type = "message"
+    )
+    expect_false(any(grepl(info_regex, non_verbose_messages)))
+  }
+
+  assert_function(travel_time_matrix)
+  assert_function(expanded_travel_time_matrix)
+  assert_function(detailed_itineraries)
+  assert_function(pareto_frontier)
+  assert_function(accessibility)
 })
