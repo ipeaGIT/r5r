c12cbfbe12491909a99c5ee736ecea2684236e8c
Daniel Herszenhut <dhersz@gmail.com>
2022-06-15 16:11:51 -0300

    more set_monte_carlo_draws tests

--- r-package/tests/testthat/setup.R
+++ r-package/tests/testthat/setup.R
@@ -16,6 +16,7 @@ if (Sys.getenv("NOT_CRAN") != "false") {
   spo_path <- system.file("extdata/spo", package = "r5r")
   spo_core <- setup_r5(spo_path, verbose = FALSE)
   spo_points <- data.table::fread(file.path(spo_path, "spo_hexgrid.csv"))
+  spo_points[, opportunities := 1]
   spo_fare_struc <- setup_fare_structure(spo_core, 5)
   spo_fare_struc$fares_per_transfer <- data.table::data.table(NULL)
 }
--- r-package/tests/testthat/test-set_monte_carlo_draws.R
+++ r-package/tests/testthat/test-set_monte_carlo_draws.R
@@ -14,12 +14,13 @@ test_that("input is correct", {
   expect_error(tester(Inf))
 })
 
-# only one monte carlo draw is performed per minute if the gtfs feeds used to
-# create the network don't include a frequencies table (independently of the
-# value set in draws_per_minute). this value is respected when any of the feeds
-# include a frequencies table
+# only one monte carlo draw is performed per minute if the routing function is
+# using the fastraptor algorithm and the gtfs feeds used to create the network
+# don't include a frequencies table (independently of the value set in
+# draws_per_minute). the number of monte carlo draws is respected when any of
+# the feeds include a frequencies table and when using the mcraptor algorithm.
 
-test_that("draws_per_minute arg works in expanded_travel_time_matrix()", {
+test_that("works in expanded_travel_time_matrix() - poa", {
   basic_expr <- call(
     "expanded_travel_time_matrix",
     r5r_core = r5r_core,
@@ -39,32 +40,211 @@ test_that("draws_per_minute arg works in expanded_travel_time_matrix()", {
   big_draws <- eval(big_draws_expr)
   expect_identical(small_draws, big_draws)
   expect_identical(unique(small_draws$draw_number), 1L)
+})
 
-  spo_basic_expr <- basic_expr
-  spo_basic_expr$r5r_core <- spo_core
-  spo_basic_expr$origins <- spo_points[51:65]
-  spo_basic_expr$destinations <- spo_points[51:65]
+test_that("works in expanded_travel_time_matrix() - spo", {
+  basic_expr <- call(
+    "expanded_travel_time_matrix",
+    r5r_core = spo_core,
+    origins = spo_points[51:65],
+    destinations = spo_points[51:65],
+    mode = c("TRANSIT", "WALK"),
+    departure_datetime = departure_datetime,
+    max_trip_duration = 60,
+    time_window = 30
+  )
 
-  spo_small_draws_expr <- spo_big_draws_expr <- spo_basic_expr
-  spo_small_draws_expr$draws_per_minute <- 1
-  spo_big_draws_expr$draws_per_minute <- 5
+  small_draws_expr <- big_draws_expr <- basic_expr
+  small_draws_expr$draws_per_minute <- 1
+  big_draws_expr$draws_per_minute <- 5
 
-  # all od pairs should include n entries per departure_time, where n the
-  # specified number of draws
+  small_draws <- eval(small_draws_expr)
+  expect_identical(unique(small_draws$draw_number), 1L)
 
-  spo_small_draws <- eval(spo_small_draws_expr)
-  expect_identical(unique(spo_small_draws$draw_number), 1L)
-
-  spo_big_draws <- eval(spo_big_draws_expr)
-  draws_summary <- spo_big_draws[
+  big_draws <- eval(big_draws_expr)
+  draws_summary <- big_draws[
     ,
     .(contains_all_draws = identical(draw_number, 1:5)),
     by = .(from_id, to_id, departure_time)
   ]
   expect_true(all(draws_summary$contains_all_draws))
 
-  # results are always different because of R5 randomization
+  big_draws_2 <- eval(big_draws_expr)
+  expect_false(identical(big_draws, big_draws_2))
+})
 
-  spo_big_draws_2 <- eval(spo_big_draws_expr)
-  expect_false(identical(spo_big_draws, spo_big_draws_2))
+# there's no way to actually know how many draws were performed with the other
+# functions, so we assume *some* draws are done and the randomization happens
+# if the results of the same expression are different in two different
+# evaluations
+
+test_that("works in travel_time_matrix() - poa", {
+  basic_expr <- call(
+    "travel_time_matrix",
+    r5r_core = r5r_core,
+    origins = pois,
+    destinations = pois,
+    mode = c("TRANSIT", "WALK"),
+    departure_datetime = departure_datetime,
+    max_trip_duration = 60,
+    time_window = 30,
+    percentiles = c(1, 50, 99)
+  )
+
+  small_draws_expr <- big_draws_expr <- basic_expr
+  small_draws_expr$draws_per_minute <- 1
+  big_draws_expr$draws_per_minute <- 5
+
+  small_draws <- eval(small_draws_expr)
+  big_draws <- eval(big_draws_expr)
+  expect_identical(small_draws, big_draws)
+
+  mcrap_basic_expr <- basic_expr
+  mcrap_basic_expr$fare_structure <- fare_structure
+  mcrap_basic_expr$max_fare <- 5
+  mcrap_small_draws_expr <- mcrap_big_draws_expr <- mcrap_basic_expr
+  mcrap_small_draws_expr$draws_per_minute <- 1
+  mcrap_big_draws_expr$draws_per_minute <- 5
+
+  mcrap_small_draws <- eval(mcrap_small_draws_expr)
+  mcrap_big_draws <- eval(mcrap_big_draws_expr)
+  expect_false(identical(mcrap_small_draws, mcrap_big_draws))
+})
+
+test_that("works in travel_time_matrix() - spo", {
+  basic_expr <- call(
+    "travel_time_matrix",
+    r5r_core = spo_core,
+    origins = spo_points[51:80],
+    destinations = spo_points[51:80],
+    mode = c("TRANSIT", "WALK"),
+    departure_datetime = departure_datetime,
+    max_trip_duration = 60,
+    time_window = 30,
+    percentiles = c(1, 50, 99)
+  )
+
+  small_draws_expr <- big_draws_expr <- basic_expr
+  small_draws_expr$draws_per_minute <- 1
+  big_draws_expr$draws_per_minute <- 5
+
+  small_draws <- eval(small_draws_expr)
+  big_draws <- eval(big_draws_expr)
+  expect_false(identical(small_draws, big_draws))
+
+  big_draws2 <- eval(big_draws_expr)
+  expect_false(identical(big_draws, big_draws2))
+})
+
+test_that("works in accessibility() - poa", {
+  basic_expr <- call(
+    "accessibility",
+    r5r_core = r5r_core,
+    origins = points[1:30],
+    destinations = points[1:30],
+    mode = c("TRANSIT", "WALK"),
+    departure_datetime = departure_datetime,
+    opportunities_colnames = "population",
+    decay_function = "step",
+    cutoffs = 30,
+    max_trip_duration = 60,
+    time_window = 30,
+    percentiles = c(1, 50, 99)
+  )
+
+  small_draws_expr <- big_draws_expr <- basic_expr
+  small_draws_expr$draws_per_minute <- 1
+  big_draws_expr$draws_per_minute <- 5
+
+  small_draws <- eval(small_draws_expr)
+  big_draws <- eval(big_draws_expr)
+  expect_identical(small_draws, big_draws)
+
+  mcrap_basic_expr <- basic_expr
+  mcrap_basic_expr$fare_structure <- fare_structure
+  mcrap_basic_expr$max_fare <- 5
+  mcrap_small_draws_expr <- mcrap_big_draws_expr <- mcrap_basic_expr
+  mcrap_small_draws_expr$draws_per_minute <- 1
+  mcrap_big_draws_expr$draws_per_minute <- 5
+
+  mcrap_small_draws <- eval(mcrap_small_draws_expr)
+  mcrap_big_draws <- eval(mcrap_big_draws_expr)
+  expect_false(identical(mcrap_small_draws, mcrap_big_draws))
+})
+
+test_that("works in accessibility() - spo", {
+  basic_expr <- call(
+    "accessibility",
+    r5r_core = spo_core,
+    origins = spo_points[51:80],
+    destinations = spo_points[51:80],
+    mode = c("TRANSIT", "WALK"),
+    departure_datetime = departure_datetime,
+    opportunities_colnames = "opportunities",
+    decay_function = "step",
+    cutoffs = 30,
+    max_trip_duration = 60,
+    time_window = 30,
+    percentiles = c(1, 50, 99)
+  )
+
+  small_draws_expr <- big_draws_expr <- basic_expr
+  small_draws_expr$draws_per_minute <- 1
+  big_draws_expr$draws_per_minute <- 5
+
+  small_draws <- eval(small_draws_expr)
+  big_draws <- eval(big_draws_expr)
+  expect_false(identical(small_draws, big_draws))
+
+  big_draws2 <- eval(big_draws_expr)
+  expect_false(identical(big_draws, big_draws2))
+})
+
+test_that("draws_per_minute arg works in pareto_frontier() - poa", {
+  basic_expr <- call(
+    "pareto_frontier",
+    r5r_core = r5r_core,
+    origins = points[1:10],
+    destinations = points[1:10],
+    mode = c("TRANSIT", "WALK"),
+    departure_datetime = departure_datetime,
+    max_trip_duration = 60,
+    time_window = 30,
+    percentiles = c(1, 50, 99),
+    fare_structure = fare_structure,
+    monetary_cost_cutoffs = c(0, 5, 10)
+  )
+
+  small_draws_expr <- big_draws_expr <- basic_expr
+  small_draws_expr$draws_per_minute <- 1
+  big_draws_expr$draws_per_minute <- 5
+
+  small_draws <- eval(small_draws_expr)
+  big_draws <- eval(big_draws_expr)
+  expect_false(identical(small_draws, big_draws))
+})
+
+# this test is currently failing
+test_that("draws_per_minute arg works in pareto_frontier() - spo", {
+  basic_expr <- call(
+    "pareto_frontier",
+    r5r_core = spo_core,
+    origins = spo_points[51:80],
+    destinations = spo_points[51:80],
+    mode = c("TRANSIT", "WALK"),
+    departure_datetime = departure_datetime,
+    max_trip_duration = 60,
+    time_window = 30,
+    percentiles = c(1, 50, 99),
+    fare_structure = spo_fare_struc,
+    monetary_cost_cutoffs = c(0, 5, 10, 15, Inf)
+  )
+
+  small_draws_expr <- big_draws_expr <- basic_expr
+  small_draws_expr$draws_per_minute <- 1
+  big_draws_expr$draws_per_minute <- 5
+
+  small_draws <- eval(small_draws_expr)
+  big_draws <- eval(big_draws_expr)
+  expect_false(identical(small_draws, big_draws))
 })
