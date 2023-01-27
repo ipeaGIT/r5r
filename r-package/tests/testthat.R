065c62f7259612cc1710414aa7f4deec780201e0
rafapereirabr <rafa.pereira.br@gmail.com>
2023-01-27 12:06:49 -0300

    do not run test_check("r5r") on CRAN

--- r-package/tests/testthat.R
+++ r-package/tests/testthat.R
@@ -1,6 +1,8 @@
-options(java.parameters = '-Xmx2G')
+if (identical(tolower(Sys.getenv("NOT_CRAN")), "true")) {
+  options(java.parameters = '-Xmx2G')
 
-library(testthat)
-library(r5r)
+  library(testthat)
+  library(r5r)
 
-test_check("r5r")
+  test_check("r5r")
+}
