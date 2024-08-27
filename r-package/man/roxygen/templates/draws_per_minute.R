6854c5887ac8210f0d198f920d1fc593206ae65b
rafapereirabr <rafa.pereira.br@gmail.com>
2024-08-27 09:30:38 -0300

    fixed typo

--- r-package/man/roxygen/templates/draws_per_minute.R
+++ r-package/man/roxygen/templates/draws_per_minute.R
@@ -3,5 +3,5 @@
 #'   when estimating accessibility. Defaults to 5. This would mean 300 draws in
 #'   a 60-minute time window, for example. This parameter only affects the
 #'   results when the GTFS feeds contain a `frequencies.txt` table. If the GTFS
-#'   feed does not have a frequency table, r5r still allow for multiple runs
+#'   feed does not have a frequency table, r5r still allows for multiple runs
 #'   over the set `time_window` but in a deterministic way.
