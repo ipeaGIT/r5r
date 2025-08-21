0d773a34c819d6bdd740fd241222d9d46962d9d3
rafapereirabr <rafa.pereira.br@gmail.com>
2025-08-21 08:49:13 -0300

    fix url in vignettes

--- r-package/NEWS.md
+++ r-package/NEWS.md
@@ -1,11 +1,11 @@
-# r5r 2.3.0 dev
+# r5r 2.3.0
 
 **Major changes**
 
 - New function `build_network()` to replace `setup_r5()`, which is being deprecated. Idiomatically `r5r_core` is now `r5r_network`.
 - New function `arrival_travel_time_matrix()` to calculate travel time matrix between origin destination pairs considering the time of arrival, instead of a depature time. Closes [#291](https://github.com/ipeaGIT/r5r/issues/291)
 - We have now implemented a reverse search optimization for direct transport modes (walking and cycling) in the functions `travel_time_matrix()`, `expanded_travel_time_matrix()` and `arrival_travel_time_matrix()`. In practice, this means that these functions are now much faster when there are multiple origins to few destinations but only when there is no elevation `.tif` file in the data path. Closes [#450](https://github.com/ipeaGIT/r5r/issues/450)
-- All routing and accessibility functions now have new parameters `new_carspeeds`, `carspeed_scale` and `new_lts`, which allow one to use custom car speeds and LTS levels for cycling, which allow one to build different scenarios of traffic congestion, road closure and interventions in cycling infrastructure. Closes [#289](https://github.com/ipeaGIT/r5r/issues/289)
+- All routing and accessibility functions in `r5r` have new parameters `new_carspeeds`, `carspeed_scale` and `new_lts` which allow one to use custom car speeds and LTS levels for cycling. These parameters provide convient and efficient ways to build different scenarios of traffic congestion, road closure and interventions in cycling infrastructure. Closes [#289](https://github.com/ipeaGIT/r5r/issues/289)
 
 **Minor changes**
 
@@ -14,6 +14,7 @@
 - Less cluttering messages in r5r dialogue. Removed logback startup messages. `Verbose=F` now completly silences java output. `Verbose=T` only reports messages up to INFO level as opposed to up to DEBUG [#456](https://github.com/ipeaGIT/r5r/pull/456).
 - Removed date from r5r-log. You no longer have to delete the previous day's log! [#456](https://github.com/ipeaGIT/r5r/pull/456)
 - Improved warning and error messages.
+- r5r developers can now set `options(r5r.r5jar=...)` to use a local JAR instead of the R5 jar downloaded by r5r.
 
 **Bug fixes**
 
--- r-package/tests/tests_rafa/osm_ids.R
+++ r-package/tests/tests_rafa/osm_ids.R
@@ -8,7 +8,7 @@ RcppParallel::setThreadOptions (numThreads = 4L) # or desired number
 
 # data path where the .pbf file is located
 data_path <- system.file("extdata/poa", package = "r5r")
-pbf_path <- paste0(data_path,'/poa_osm.pbf')
+pbf_path <- file.path(data_path,'/poa_osm.pbf')
 
 # read layer of lines from pbf
 edges <- sf::st_read(
@@ -23,8 +23,13 @@ nodes <- sf::st_read(
   quiet = TRUE
 )
 
+edges$oneway <- ifelse(
+  edges$other_tags %like% "\"oneway\"=>\"yes\"",
+  "yes",
+  NA)
+
 # keep only osm ids
-edges <- edges[,c("osm_id","highway")]
+edges <- edges[,c("osm_id","highway", "oneway")]
 nodes <- nodes[,c("osm_id","highway")]
 
 
@@ -40,65 +45,75 @@ data.table::setDT(car_network)
 car_network[, from_id := as.numeric(from_id)]
 car_network[, to_id := as.numeric(to_id)]
 
-# create a unique edge id with the first and last node of the edge
-car_network <- car_network |>
-  group_by(way_id)  |>
-  mutate(unique_id_forward = paste0(min(from_id,to_id), '-', max(from_id,to_id)),
-         unique_id_backward = paste0(max(from_id,to_id), '-', min(from_id,to_id))
+# unique id, mas a edge_id ja eh uma unique id de cada edge
+car_network[,
+            unique_id := paste(way_id, geom_num, edge_id, from_id, to_id, sep = "_"),
+            by = .I
+            ]
+
+head(car_network)
+
+
+data.table::setDT(edges)
+car_network[edges, on = .(way_id=osm_id), oneway := i.oneway]
+car_network$oneway
+
+# 26786730 one way
+# 27126547 two ways
+car_network2 <- car_network[way_id %in% c(26786730, 27126547), ]
+car_network2 <- car_network
+
+# edges_sf <- sf::st_sf(edges)
+# mapview::mapview(
+#   filter(edges_sf, osm_id==27126547)
+# )
+
+one_way_net <- car_network2 |>
+  filter(oneway=="yes") |>
+  rename(osm_id = way_id) |>
+  group_by(osm_id, oneway)  |>
+  summarise(from_id = min(from_id),
+            from_lat = from_lat[which.min(from_id)],
+            from_lon = from_lon[which.min(from_id)],
+            to_id =  max(to_id),
+            to_lat = from_lat[which.max(to_id)],
+            to_lon = from_lon[which.max(to_id)]
+            ) |>
+  ungroup()
+
+
+two_way_net_forwards <- car_network2 |>
+  filter(is.na(oneway)) |>
+  rename(osm_id = way_id) |>
+  group_by(osm_id)  |>
+  summarise(oneway = "forwards",
+            from_id = min(from_id),
+            from_lat = from_lat[which.min(from_id)],
+            from_lon = from_lon[which.min(from_id)],
+            to_id =  max(to_id),
+            to_lat = from_lat[which.max(to_id)],
+            to_lon = from_lon[which.max(to_id)]
   ) |>
   ungroup()
 
-# create unique id where A-B equals B-A to identify two way segments
-car_network <- car_network |>
-  rowwise()  |>
-  mutate(permut_id = paste0(min(from_id,to_id), '-', max(from_id,to_id))
-  )
-
-# select columns
-df <- car_network |>
-  dplyr::select(osm_id = way_id, unique_id_forward, unique_id_backward, permut_id,
-                distance_m = d, time_s = time,
-                from_id, to_id, from_lon, from_lat, to_lon, to_lat)
-
-head(df)
 
 
-# keep only the first observation of each edge
-data.table::setDT(df)
-df <- df[, .SD[1,], by=.(osm_id ,permut_id)]
-
-head(df)
+two_way_net_backwards <- car_network2 |>
+  filter(is.na(oneway)) |>
+  rename(osm_id = way_id) |>
+  group_by(osm_id)  |>
+  summarise(oneway = "backwards",
+            from_id = max(from_id),
+            from_lat = from_lat[which.max(from_id)],
+            from_lon = from_lon[which.max(from_id)],
+            to_id =  min(to_id),
+            to_lat = from_lat[which.min(to_id)],
+            to_lon = from_lon[which.min(to_id)]
+  ) |>
+  ungroup()
 
 
 
 
-
-
-
-tttttt <- 835202042
-
-t <- df |>
-  filter(osm_id==tttttt)
-
-t[, .(from_id = min(from_id),
-      to_id = max(to_id),
-      distance_m = sum(distance_m),
-      time_s  = sum(time_s),
-      from_lon = from_lon[which.min(from_id)],
-      from_lat = from_lat[which.min(from_id)],
-      to_lon = to_lon[which.max(from_id)],
-      to_lat = to_lat[which.max(from_id)]
-      ),
-  by = .(osm_id, unique_id )]
-
-edges |>
-  filter(osm_id==tttttt) |>
-  sf::st_length()
-
-duvida.
-no arcgis a gente pede apenas A-B, ou pede tambem B-A ?
-a gente soh precisa do weight factor.... unidirecional
-mas se a gente pedir a-b e somente for permitiodo b-a,
-dai a rota e tempo de viagem vai ser meio loucae a gente
-vai acabar tendo q rodar de novo
-entao melhor rodar rodo a-b e b-a
+my_net <- rbind(one_way_net, two_way_net_forwards, two_way_net_backwards)
+my_net
