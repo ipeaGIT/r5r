178711e78449f7e8ab0cea66d16fd15f59386908
Matt Bhagat-Conway <matt@indicatrix.org>
2026-01-07 12:49:31 -0500

    automated tests for #516

--- r-package/tests/testthat/test-build_network.R
+++ r-package/tests/testthat/test-build_network.R
@@ -24,9 +24,6 @@ test_that("build_network - expected behavior", {
 
 })
 
-
-
-
 # Expected errors
 test_that("build_network - expected errors", {
 
@@ -111,3 +108,106 @@ test_that("throws error if Java is not 21", {
 #
 #   )
 # }
+
+test_that("build_network - feeds with errors", {
+  # create a feed with an error
+  temp_net_dir <- tempfile()
+  dir.create(temp_net_dir, mode = "0700")
+  gtfs_dir <- file.path(temp_net_dir, "gtfs")
+  dir.create(gtfs_dir, mode = "0700")
+
+  # network with medium priority errors: empty frequencies.txt
+  # should not prevent network build
+  net_medium_dir <- file.path(temp_net_dir, "network_medium")
+  dir.create(net_medium_dir, mode = "0700")
+  src_net_path <- system.file("extdata/poa", package = "r5r")
+  unzip(file.path(src_net_path, "poa_eptc.zip"), exdir = gtfs_dir)
+  file.create(file.path(gtfs_dir, "frequencies.txt"))
+
+  # turn back to GTFS
+  zip(
+    file.path(net_medium_dir, "gtfs.zip"),
+    file.path(gtfs_dir, list.files(gtfs_dir)),
+    flags = "-j" # don't record directory names
+  )
+
+  file.copy(
+    file.path(src_net_path, "poa_osm.pbf"),
+    file.path(net_medium_dir, "osm.pbf")
+  )
+
+  # build network
+  testthat::expect_message(
+    {net <- build_network(net_medium_dir, temp_dir = TRUE)},
+    # current R5 (7.4) records the empty table error twice ?
+    regex = "2 errors found in GTFS"
+  )
+
+  errors <- get_gtfs_errors(net)
+
+  testthat::expect_equal(
+    errors,
+    # note that the expected table is dependent on R5 internals. If this test
+    # starts failing, check to see if the actual errors have the same meaning as
+    # the expected error and update accordingly.
+    data.table::data.table(
+      # current R5 (7.4) records the empty table error twice ?
+      V1 = c(1, 2),
+      file = "frequencies",
+      line = 0,
+      type = "EmptyTableError",
+      field = NA,
+      id = NA,
+      priority = "MEDIUM"
+    )
+  )
+
+  # network with high priority errors: missing stops
+  net_high_dir <- file.path(temp_net_dir, "network_high")
+  dir.create(net_high_dir, mode = "0700")
+
+  # clean up from medium
+  file.remove(file.path(gtfs_dir, "frequencies.txt"))
+  trips <- read.csv(file.path(gtfs_dir, "trips.txt"))
+  write.csv(
+    trips[-1,],
+    file.path(gtfs_dir, "trips.txt")
+  )
+  
+  # turn back to GTFS
+  zip(
+    file.path(net_high_dir, "gtfs.zip"),
+    file.path(gtfs_dir, list.files(gtfs_dir)),
+    flags = "-j" # don't record directory names
+  )
+
+  file.copy(
+    file.path(src_net_path, "poa_osm.pbf"),
+    file.path(net_high_dir, "osm.pbf")
+  )
+
+  # build network
+  testthat::expect_error(
+    build_network(net_high_dir, temp_dir = TRUE),
+    # current R5 (7.4) records the empty table error twice ?
+    regex = "High priority GTFS errors found; network build failed."
+  )
+
+  errors <- get_gtfs_errors(net_high_dir)
+  testthat::expect_equal(
+    errors,
+    # note that the expected table is dependent on R5 internals. If this test
+    # starts failing, check to see if the actual errors have the same meaning as
+    # the expected error and update accordingly.
+    data.table::data.table(
+      # current R5 short-circuits after high priority error so we only get one
+      V1 = 1,
+      file = "stop_times",
+      line = 2,
+      type = "ReferentialIntegrityError",
+      field = "trip_id",
+      id = NA,
+      priority = "HIGH"
+    )
+  )
+})
