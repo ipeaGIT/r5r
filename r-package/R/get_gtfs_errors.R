606a2d16967c060ea81e335f0ea1c34c69cb68c9
rafapereirabr <rafa.pereira.br@gmail.com>
2026-01-08 23:30:18 -0300

    docs get_gtfs_errors

--- r-package/R/get_gtfs_errors.R
+++ r-package/R/get_gtfs_errors.R
@@ -1,13 +1,26 @@
-#' Get GTFS errors encountered in network building
-#' 
-#' This returns a data frame of GTFS errors R5 encountered when building the network.
-#' You can call this either with the network itself as the main parameter. If network
-#' build fails, you won't have a network object, so you can also call this with the
-#' path to where the network is stored.
-#' 
+#' Get GTFS eventual errors encountered in network building
+#'
+#' This returns a data frame of GTFS errors R5 encountered when building the
+#' network. You can call this with the network itself as the main parameter. If
+#' network build fails, you won't have a network object, so you can also call
+#' this with the `data_path` to where the network is stored.
+#'
 #' @param r5r_network the R5R network object, or a path to the location where
 #'     the network is stored (useful if network build failed).
-#' 
+#'
+#' @return A `data.frame`
+#'
+#' @family support functions
+#'
+#' @examplesIf identical(tolower(Sys.getenv("NOT_CRAN")), "true")
+#' library(r5r)
+#'
+#' # directory with street network and gtfs files
+#' data_path <- system.file("extdata/poa", package = "r5r")
+#' r5r_network <- build_network(data_path)
+#'
+#' get_gtfs_errors(r5r_network)
+#'
 #' @export
 get_gtfs_errors <- function (r5r_network) {
     checkmate::assert_multi_class(r5r_network, c("r5r_network", "character"))
