8ecccb9490fcc3bf9e98db8087f7a679d6020173
Alex Magnus <a2005magnus@gmail.com>
2026-05-16 16:40:42 +0200

    bug: fix elevation data getting deleted when making a copy of the network
    DummyScenario is used to make copies of the transport network via scenarioCopy. For some reason using affectsXLayer=true seems to break elevation data, however switching it to false restores the elevation data.

--- java-r5rcore/src/org/ipea/r5r/Scenario/DummyScenario.java
+++ java-r5rcore/src/org/ipea/r5r/Scenario/DummyScenario.java
@@ -5,12 +5,12 @@ import com.conveyal.r5.analyst.scenario.Scenario;
 public class DummyScenario extends Scenario {
     @Override
     public boolean affectsStreetLayer() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean affectsTransitLayer() {
-        return true;
+        return false;
     }
 
 }
